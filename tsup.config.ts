30928d8bfe967fc3bd7f043e5c30b70d5a68fa96
vitu <vitorafael.r.c@gmail.com>
2026-03-10 00:50:46 -0300

    bump version to 1.0.1 and update package keywords

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   clean: true,
-  loader: { '.mp3': 'copy' },
-})
+  loader: { ".mp3": "copy" },
+  esbuildOptions(options) {
+    options.assetNames = "sounds/[name]";
+  },
+  outExtension({ format }) {
+    return { js: format === "esm" ? ".esm.js" : ".js" };
+  },
+});
